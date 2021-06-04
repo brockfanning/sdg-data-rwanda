f2f0758aa1fdadb5b1e2144adfc9c90637dfb6c5
Brock Fanning <brockfanning@gmail.com>
2021-06-04 19:45:54 -0400

    Extra output for use in SDG Lab

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -1,5 +1,6 @@
 from sdg.open_sdg import open_sdg_build
-
+import sdg
+import os
 
 def alter_meta(meta):
 # Automatically detect global indicators.
@@ -15,3 +16,23 @@ def alter_meta(meta):
     return meta
 
 open_sdg_build(config='config_data.yml', alter_meta=alter_meta)
+
+# Also produce a filtered SDMX output for use by UNSD.
+global_dsd = 'https://registry.sdmx.org/ws/public/sdmxapi/rest/datastructure/IAEG-SDGs/SDG/latest/?format=sdmx-2.1&detail=full&references=children'
+
+data_input = sdg.inputs.InputSdmxMl_Multiple(
+    path_pattern='sdmx-data/*.xml',
+    import_codes=True,
+    dsd='RWA_C_V2_SDG DSD.xml',
+    drop_singleton_dimensions=False,
+)
+inputs = [data_input]
+schema = sdg.schemas.SchemaInputOpenSdg(schema_path='_prose.yml')
+output = sdg.outputs.OutputSdmxMl(inputs, schema,
+    output_folder=os.path.join('_site', 'unsd'),
+    sender_id='Rwanda',
+    structure_specific=True,
+    constrain_data=True,
+    dsd=global_dsd,
+)
+output.execute()
